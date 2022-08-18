86a88363609d9b02a00dbaf3aa75c4bda2726ce2
Teja ganji <saitejaganji999@gmail.com>
2022-08-18 10:06:42 +0000

    react code 2

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div class="congrats-card-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <h1 className="card-title">Kiran V</h1>
+      <p className="card-description">
+        Vishnu institute of computer Education and Technology Lb nagar
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
